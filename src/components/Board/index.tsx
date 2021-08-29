6f2d39e830638932c9186bd5f4384192369a5cbd
Amol Rama <43121460+AmoleR@users.noreply.github.com>
2021-08-29 15:09:05 -0700

    feat(README): add a readme
    Adding a README about AsyPad and Development guidelines

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,8 +2,6 @@ import { FC, useContext, useEffect, useRef } from "react";
 import { ButtonContext } from "context";
 import ActionHandler from "./actions";
 
-type Coords = { x: number; y: number };
-
 const Board: FC<{}> = () => {
   const canvas = useRef<HTMLCanvasElement | null>(null);
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
